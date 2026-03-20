12cee6cf6c2d540769295b32edca4a9d403e8a37
Tu Nombre de Usuario de GitHub <johansneyderguataqui@gmail.com>
2026-03-19 20:53:08 -0500

    Agrego clases Contador

---
+++ Contador.cs
@@ -0,0 +1,28 @@
+using System;
+
+public class Class1
+{
+    public Class1()
+    {
+        private int contador;
+    
+
+        public Contador()
+        {
+            this.contador = 0;
+        }
+
+        public int incrementar()
+        {
+            contador++;
+            return contador;
+        }
+
+        public int decrementar()
+        {
+            contador--;
+            return contador;
+        }
+    }
+}
+}
